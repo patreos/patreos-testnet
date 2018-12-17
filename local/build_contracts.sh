dd1b8b6e0f8d01780003734f7637b6d82a880af4
okayplanet <tyrick@gmail.com>
2018-12-16 21:09:50 -0700

    build contracts updated to use jq

--- local/build_contracts.sh
+++ local/build_contracts.sh
@@ -3,9 +3,14 @@ source ~/dev/patreos/patreos-tests/local/constants.sh
 
 echo "Compiling Patreos Contracts"
 pushd ~/dev/patreos/patreos-contracts/
-for contract in "${PATREOS_CONTRACTS[@]}"
-do
-  echo "Compiling $contract"
-  ./generate.sh $contract
+
+users=$(echo "$USER_KEYS_JSON" | jq '.contracts')
+for row in $(echo "${users}" | jq -r '.[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+    contract=$(_jq '.name')
+    echo "Compiling $contract"
+    ./generate.sh $contract
 done
 popd
